e22266e8191adf1180d8ceaf48b0b2e9d8c74650
Hampton Lab <waseem.mehany@hamptonlab.com>
2023-11-23 18:57:38 -0800

    Added a mobile nav and contact page template

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,15 @@ module.exports = {
 			},
 			colors: {
 				"gray": 'rgba(244, 244, 244, 0.8)',
+				"gray-200": "rgba(244, 244, 244, 0.95)",
 				// "blue-gradient-1": "rgb(60, 120, 235)",
 				// "blue-gradient-2": "rgb(90, 190, 245)"
 				"hampton-blue-1": "#3B7EF2",
 				"hampton-blue-2": "#409CEB",
 				"hampton-blue-3": "#46BEE2",
+				"hampton-blue-1-transparent": "rgba(59, 126, 242, 0.25)",
+				"hampton-blue-2-transparent": "rgba(64, 156, 235, 0.25)",
+				"hampton-blue-3-transparent": "rgba(70, 190, 226, 0.25)",
 				"hampton-white-1": "rgba(255, 255, 255, 0.8)"
 			},
 		},
